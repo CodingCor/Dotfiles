c9abb85a2e1d22578ece9152daac1d81a486a3a5
CodingCor <michael.mayer2105@gmail.com>
2023-06-13 21:43:31 +0200

    Initial Push

---
+++ .config/zsh/zsh-alias.zsh
@@ -0,0 +1,6 @@
+alias ls='ls --color=auto --group-directories-first'
+alias ll='ls -la'
+
+alias grep='grep --color=auto'
+alias lvim='nvim -u ~/.config/nvim_lua/init.lua'
+alias dotgit='/usr/bin/git --git-dir=/home/me/.dotgit/ --work-tree=/home/me'
